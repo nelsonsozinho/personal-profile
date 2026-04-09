0130176928c36011e246fcfb366c37e22067bd52
nelsonsozinho <nelsonsozinho@gmail.com>
2026-04-09 15:01:03 -0400

    bio improvement

--- src/app/features/home/sections/presentation-section/presentation-section.component.ts
+++ src/app/features/home/sections/presentation-section/presentation-section.component.ts
@@ -11,10 +11,20 @@ import { RouterLink } from '@angular/router';
 export class PresentationSectionComponent {
   protected readonly name = 'Nelson Sozinho';
   protected readonly role = 'Backend Engineer';
-  protected readonly shortBio =
-    'Seasoned software developer with deep expertise in the Java ecosystem and AWS cloud technologies. ' +
-    'Experienced across finance, education, and marketing industries, delivering scalable APIs, ' +
-    'event-driven systems, and cloud-native solutions.';
+
+  protected readonly shortBio1 =
+    'I’m a Senior Software Engineer. I enjoy everything related to software development, whether it’s ' +
+    'code, tools, frameworks, programming languages, platforms, and more. I have extensive experience ' +
+    'and am constantly seeking to improve myself in this field.';
+
+  protected readonly shortBio2 =
+    'I love learning new things, especially those that can help me in my daily life and in my profession.';
+
+  protected readonly shortBio3 =
+    'In my spare time, I also enjoy learning new things like, new technologies and challenging myself with new projects.';
+
+  protected readonly shortBio4 =
+    "I'm always open to collaboration and excited about opportunities where I can contribute, learn, and grow professionally.";
 
   protected readonly photoUrl = '/assets/profile_image.png';
   protected readonly email = 'nelsonsozinho@gmail.com';
